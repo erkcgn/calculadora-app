22547dbdd2683b8a362b26d145264ecaae677f97
Erika <erikac.ec80@gmail.com>
2022-04-29 10:40:47 -0300

    form en componente hijo

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.css']
 })
-export class FormularioComponent implements OnInit {
+export class FormularioComponent{
+  
+  operandoA: number = 0;
+  operandoB: number = 0;
 
-  constructor() { }
+  @Output() resultadoSuma = new EventEmitter<number>();
 
-  ngOnInit(): void {
+  sumar():void{
+    let resultado = this.operandoA + this.operandoB;
+    this.resultadoSuma.emit(resultado);
   }
-
 }
